42f4baff8ef4214be35e658d13a0d6480b64869c
Ethan Brown <eb934053@gmail.com>
2024-11-19 11:41:19 -0600

    Added object creation module

---
+++ src/scripts/PotteryWheel.js
@@ -0,0 +1,13 @@
+let id = 0
+
+export const makePottery = (shape, weight, height) => {
+    const potteryPiece = [
+        {
+            shape: shape,
+            weight: weight,
+            height: height,
+            id: id++
+        }
+    ]
+    return potteryPiece
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,13 @@
 // Imports go first
-
+import { makePottery } from "./PotteryWheel.js"
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
-
+const mug = makePottery("cup", 3, 4)
+const frame = makePottery("picture frame", 8, 10)
+const necklace = makePottery("bead necklace", 2, 2)
+const figurine = makePottery("portrait figure", 8, 20)
+const broom = makePottery("replica", 6, 9)
 
 // Fire each piece of pottery in the kiln
 
